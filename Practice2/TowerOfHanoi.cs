f24bd289abfc34e791f56921c078ef3a7ea908f6
SystemFalse <237849999+anton-gudchenko@users.noreply.github.com>
2026-03-03 01:13:42 +0300

    Добавлен проект для второй практической работы
    В классе TowerOfHanoi написано решение задачи о Ханойской башне в виде рекурсии и в виде цикла.

---
+++ Practice2/TowerOfHanoi.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+namespace Practice2
+{
+    public static class TowerOfHanoi
+    {
+        public record Move(int FromPivot, int ToPivot);
+
+        private static void RecursiveSolve(List<Move> solution, int n, int fromPivot, int toPivot)
+        {
+            if (n == 1)
+            {
+                solution.Add(new(fromPivot, toPivot));
+                return;
+            }
+            int midPivot = 3 - (fromPivot + toPivot);
+            RecursiveSolve(solution, n - 1, fromPivot, midPivot);
+            solution.Add(new(fromPivot, toPivot));
+            RecursiveSolve(solution, n - 1, midPivot, toPivot);
+        }
+
+        public static List<Move> RecursiveSolve(int initialDisks)
+        {
+            if (initialDisks < 1)
+            {
+                throw new ArgumentException("invalid initial disk count");
+            }
+            List<Move> solution = [];
+            RecursiveSolve(solution, initialDisks, 0, 2);
+            return solution;
+        }
+
+        private class StackInstance
+        {
+            public required int N { get; set; }
+            public required int FromPivot { get; set; }
+            public required int ToPivot { get; set; }
+            public int MidPivot { get => 3 - (FromPivot + ToPivot); }
+            public int RecursiveCall { get; set; }
+
+            public Move Move
+            {
+                get => new(FromPivot, ToPivot);
+            }
+        }
+
+        public static List<Move> LoopSolve(int initialDisks)
+        {
+            List<Move> solution = [];
+            Stack<StackInstance> stack = [];
+            stack.Push(new()
+            {
+                N = initialDisks,
+                FromPivot = 0,
+                ToPivot = 2
+            });
+            while (stack.Count > 0)
+            {
+                var instant = stack.Peek();
+                if (instant.N == 1)
+                {
+                    solution.Add(instant.Move);
+                    stack.Pop();
+                    continue;
+                }
+                if (instant.RecursiveCall == 0)
+                {
+                    stack.Push(new()
+                    {
+                        N = instant.N - 1,
+                        FromPivot = instant.FromPivot,
+                        ToPivot = instant.MidPivot
+                    });
+                    instant.RecursiveCall++;
+                }
+                else if (instant.RecursiveCall == 1)
+                {
+                    solution.Add(instant.Move);
+                    stack.Push(new()
+                    {
+                        N = instant.N - 1,
+                        FromPivot = instant.MidPivot,
+                        ToPivot = instant.ToPivot
+                    });
+                    instant.RecursiveCall++;
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+            return solution;
+        }
+    }
+}
